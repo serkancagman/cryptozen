426917a472710ff17ebd24ed3d8bc1a54a82a3ab
serkancagman <serkancagmann@gmail.com>
2022-05-01 18:21:35 +0300

    Add buy menu items

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,9 +1,30 @@
-import React from 'react'
-import style from "./Style/Header.module.css"
+import React from "react";
+import { Link } from "react-router-dom";
+import style from "./Style/Header.module.css";
+import logo from "Assets/Logo/logo.png";
+import { Dropdown } from "antd";
+import { buyList } from "./NavbarItems/NavbarItems";
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <header className={style.header}>
+      <div className="container-fluid">
+        <nav className="navbar navbar-expand-lg ">
+          <Link to="/" className="navbar-brand">
+            <img src={logo} className={style.logo} alt="logo" />
+          </Link>
+          <ul className="navbar-nav ms-2">
+            <li className={style.navItem}>
+              <Dropdown placement="bottomCenter" overlay={buyList}>
+                <span className={style.dropdownLink} href="/#">
+                  Buy Crypto
+                </span>
+              </Dropdown>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
+};
 
-export default Header
+export default Header;
--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "antd/dist/antd.min.css";
+import "./Style/main.css";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
