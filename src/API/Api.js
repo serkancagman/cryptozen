497d42f88befc607d19b9e5543dad036c41dd9b7
serkancagman <serkancagmann@gmail.com>
2022-05-04 18:31:25 +0300

    Finish search component

--- src/API/Api.js
+++ src/API/Api.js
@@ -27,3 +27,10 @@ export const searchCoin = async (query) => {
   );
   return res.data;
 };
+
+export const getDefaultSearchData = async (currency) => {
+  const res = await axios.get(
+    `${process.env.REACT_APP_API_URL}coins/markets?vs_currency=${currency}&order=market_cap_desc&per_page=8&page=1&sparkline=false&price_change_percentage=24h`
+  );
+  return res.data;
+};
--- src/Components/Header/NavbarItems/Search.js
+++ src/Components/Header/NavbarItems/Search.js
@@ -2,36 +2,120 @@ import React from "react";
 import style from "../Style/Header.module.css";
 import { FaSearchDollar } from "react-icons/fa";
 import { Dropdown } from "antd";
-import { searchCoin } from "API/Api";
+import { searchCoin, getDefaultSearchData } from "API/Api";
+import { Link } from "react-router-dom";
+import { useQuery } from "react-query";
 const Search = () => {
   const [isVisible, setIsVisible] = React.useState(false);
-  const [searchItems,setSearchItems] = React.useState([])
+  const [searchItems, setSearchItems] = React.useState([]);
   const [searchValue, setSearchValue] = React.useState("");
+  const [defaultValue, setDefaultValue] = React.useState([]);
+  const { data } = useQuery("defaultSearchData", () =>
+    getDefaultSearchData("USD")
+  );
   const handleVisibleChange = (flag) => {
     setIsVisible(flag);
   };
   const getSearchData = async (val) => {
     const res = await searchCoin(val);
-    setSearchItems(res)
+    setSearchItems(res.coins);
   };
   React.useEffect(() => {
     if (searchValue.length > 1) {
-      getSearchData(searchValue)
+      getSearchData(searchValue);
+    } else {
+      setSearchItems([]);
     }
   }, [searchValue]);
-console.log(searchItems);
+
+  React.useEffect(() => {
+    if (data) {
+      setDefaultValue(data);
+    }
+  }, [data]);
+
   const menu = (
     <div className={style.navSubWrapper}>
-      <div className={style.navSearchInner}>
-        <div className={style.navSearch}>
-          <input
-            value={searchValue}
-            onChange={(e) => setSearchValue(e.target.value)}
-            type="text"
-            className={style.navSearchInput}
-            placeholder="Search"
-          />
+      <div className={style.searchWrapper}>
+        <div className={style.navSearchInner}>
+          <div className={style.navSearch}>
+            <input
+              value={searchValue}
+              onChange={(e) => setSearchValue(e.target.value)}
+              type="text"
+              className={style.navSearchInput}
+              placeholder="Search"
+            />
+          </div>
         </div>
+        <div className={style.navSearchTitle}>
+          <span className={style.navSearchTitleText}>
+            {searchItems.length > 0 ? "Search Results" : "Hot Searches"}
+            {searchItems.length === 0 && (
+              <span className={style.navLinkTag}>HOT</span>
+            )}
+          </span>
+        </div>
+        <ul className={style.searchList}>
+          {searchItems.length > 1
+            ? searchItems.map((item, index) => {
+                return (
+                  <li key={index} className={style.searchListItem}>
+                    <Link
+                      className={style.searchItemWrapper}
+                      to={`/coin/${item.id}`}
+                    >
+                      <div className="d-flex justify-content-between align-items-center">
+                        <div className="d-flex justify-content-center align-items-center">
+                          <div className={style.searchListIconWrapper}>
+                            <img
+                              src={item.thumb}
+                              alt="coin"
+                              className={style.coinIcon}
+                            />
+                          </div>
+                          <span className={style.searchListCoinName}>
+                            {item.symbol}
+                          </span>
+                        </div>
+                        <span className={style.coinRank}>
+                          # {item.market_cap_rank}
+                        </span>
+                      </div>
+                    </Link>
+                  </li>
+                );
+              })
+            : defaultValue &&
+              defaultValue.map((item, index) => {
+                return (
+                  <li key={index} className={style.searchListItem}>
+                    <Link
+                      className={style.searchItemWrapper}
+                      to={`/coin/${item.id}`}
+                    >
+                      <div className="d-flex justify-content-between align-items-center">
+                        <div className="d-flex justify-content-center align-items-center">
+                          <div className={style.searchListIconWrapper}>
+                            <img
+                              src={item.image}
+                              alt="coin"
+                              className={style.coinIcon}
+                            />
+                          </div>
+                          <span className={style.searchListCoinName}>
+                            {item.symbol}
+                          </span>
+                        </div>
+                        <span className={style.coinRank}>
+                          # {item.market_cap_rank}
+                        </span>
+                      </div>
+                    </Link>
+                  </li>
+                );
+              })}
+        </ul>
       </div>
     </div>
   );
