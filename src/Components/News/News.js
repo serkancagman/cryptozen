af4e1d1401228fb5b84b346a65f62d2ac2cc5aaa
serkancagman <serkancagmann@gmail.com>
2022-05-12 18:31:19 +0300

    Translate news component

--- src/Components/News/News.js
+++ src/Components/News/News.js
@@ -5,32 +5,34 @@ import newsImg from "Assets/News/news-1.png";
 import newsImg2 from "Assets/News/news-2.jpg";
 import newsImg3 from "Assets/News/news-3.png";
 import NewsItem from "./NewsItem";
+import { useTranslation } from "react-i18next";
 const News = () => {
+  const { t } = useTranslation();
   return (
     <section className={style.news}>
       <div className="container">
         <div className={style.newsHeader}>
-          <HeaderTag title="Latest News" />
-          <h2 className={style.newsTitle}>Recent Blog Post</h2>
+          <HeaderTag title={t('news.tag_text')} />
+          <h2 className={style.newsTitle}>{t('news.main_title')}</h2>
         </div>
         <div className="row g-3">
           <NewsItem
             imgUrl={newsImg}
-            date="Jan 20, 2022"
-            title="How to invest in ICO"
-            text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exerc"
+            date={t('news.items.first.date')}
+            title={t('news.items.first.title')}
+            text={t('news.items.first.description')}
           />
           <NewsItem
             imgUrl={newsImg2}
-            date="Jan 23, 2022"
-            title="Cryptocash is a clean & modern coin"
-            text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exerc"
+            date={t('news.items.second.date')}
+            title={t('news.items.second.title')}
+            text={t('news.items.second.description')}
           />
           <NewsItem
             imgUrl={newsImg3}
-            date="Jan 27, 2022"
-            title="Pros & Cons of Premined Crypto"
-            text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exerc"
+            date={t('news.items.third.date')}
+            title={t('news.items.third.title')}
+            text={t('news.items.third.description')}
           />
         </div>
       </div>
--- src/Components/News/NewsItem.js
+++ src/Components/News/NewsItem.js
@@ -2,7 +2,9 @@ import React from "react";
 import style from "./Style/News.module.css";
 import { BsCalendar4Week, BsPlusLg } from "react-icons/bs";
 import { Link } from "react-router-dom";
+import { useTranslation } from "react-i18next";
 const NewsItem = ({ imgUrl, date, title, text }) => {
+  const { t } = useTranslation();
   return (
     <div className="col-md-6 col-lg-4">
       <div className={style.newsItem}>
@@ -18,7 +20,7 @@ const NewsItem = ({ imgUrl, date, title, text }) => {
           <h3 className={style.newsItemTitle}>{title}</h3>
           <p className={style.newsItemText}>{text}</p>
           <Link to="/news" className={style.newsItemLink}>
-            Read More <BsPlusLg />
+            {t("news.btn_text")} <BsPlusLg />
           </Link>
         </div>
       </div>
