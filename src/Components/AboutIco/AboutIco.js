04aba9684e318dd513aba931862e53863f784f2b
serkancagman <serkancagmann@gmail.com>
2022-05-12 16:26:04 +0300

    Translate about_ico component

--- src/Components/AboutIco/AboutIco.js
+++ src/Components/AboutIco/AboutIco.js
@@ -6,17 +6,17 @@ import icon from "Assets/AboutIco/icon.png";
 import icon2 from "Assets/AboutIco/icon2.png";
 import icon3 from "Assets/AboutIco/icon3.png";
 import HeaderTag from "Components/HeaderTag/HeaderTag";
+import { useTranslation } from "react-i18next";
 const AboutIco = () => {
+  const {t} = useTranslation();
   return (
     <section className={style.aboutIco}>
       <div className="container">
         <div className={style.aboutIcoHeader}>
-          <HeaderTag title="About ICO" />
-          <h2 className={style.aboutIcoTitle}> About Crypto ICO </h2>
+          <HeaderTag title={t('about_ico.tag_text')} />
+          <h2 className={style.aboutIcoTitle}>{t('about_ico.main_title')}</h2>
           <p className={style.aboutIcoDesc}>
-            {" "}
-            Monotonectally productivate virtual benefits vis-a-vis clicks ship.
-            Seamlessly generate user friendly{" "}
+            {t('about_ico.description')}
           </p>
         </div>
         <div className="row align-items-center justify-content-center g-3">
@@ -35,10 +35,9 @@ const AboutIco = () => {
                   <img src={icon} alt="icon" className="img-fluid" />
                 </div>
                 <div className="d-flex justify-content-center align-items-start flex-column">
-                  <h4 className={style.aboutIcoItemTitle}> Money Exchange </h4>
+                  <h4 className={style.aboutIcoItemTitle}>{t('about_ico.items.first.title')}</h4>
                   <p className={style.aboutIcoItemDesc}>
-                    Continually pontificate corporate deliverables through
-                    clicks-and-mortar intellectual.
+                  {t('about_ico.items.first.description')}
                   </p>
                 </div>
               </div>
@@ -49,11 +48,10 @@ const AboutIco = () => {
                 <div className="d-flex justify-content-center align-items-start flex-column">
                   <h4 className={style.aboutIcoItemTitle}>
                     {" "}
-                    Balance Transfer{" "}
+                    {t('about_ico.items.second.title')}{" "}
                   </h4>
                   <p className={style.aboutIcoItemDesc}>
-                    Continually pontificate corporate deliverables through
-                    clicks-and-mortar intellectual.
+                  {t('about_ico.items.second.description')}
                   </p>
                 </div>
               </div>
@@ -62,10 +60,9 @@ const AboutIco = () => {
                   <img src={icon3} alt="icon" className="img-fluid" />
                 </div>
                 <div className="d-flex justify-content-center align-items-start flex-column">
-                  <h4 className={style.aboutIcoItemTitle}> Safe Transfer </h4>
+                  <h4 className={style.aboutIcoItemTitle}>{t('about_ico.items.thirth.title')}</h4>
                   <p className={style.aboutIcoItemDesc}>
-                    Continually pontificate corporate deliverables through
-                    clicks-and-mortar intellectual.
+                  {t('about_ico.items.thirth.description')}
                   </p>
                 </div>
               </div>
