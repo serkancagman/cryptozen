bdb927a46d37a4944f51c7db7aa4b6800bcacd0f
serkancagman <serkancagmann@gmail.com>
2022-05-12 15:33:28 +0300

    Translate countdown

--- src/Components/Banner/Countdown.js
+++ src/Components/Banner/Countdown.js
@@ -1,40 +1,40 @@
 import React from "react";
 import style from "./Style/Banner.module.css";
 import { useCountdown } from "Hooks/useCount";
+import { useTranslation } from "react-i18next";
 const Countdown = () => {
   const expiryTime = "23 aug 2022 15:30:25";
-
   const countdownDateTime = new Date(expiryTime).getTime();
   const [days, hours, minutes, seconds] = useCountdown(countdownDateTime);
-
+  const { t } = useTranslation();
   return (
     <div className={style.countdown}>
       <div className="d-flex flex-column align-items-center justify-content-center">
-        <h6 className={style.countdownTitle}> Token Sale Start Now In </h6>
+        <h6 className={style.countdownTitle}> {t("countdown.main_title")}</h6>
         <div className="d-flex align-items-center justify-content-center">
           <div className={style.countdownBox}>
             <span className={`${style.countdownNumber} ${style.days}`}>
               {days}
             </span>
-            <span className={style.countdownText}>DAYS</span>
+            <span className={style.countdownText}>{t("countdown.day")}</span>
           </div>
           <div className={style.countdownBox}>
             <span className={`${style.countdownNumber} ${style.hours}`}>
               {hours}
             </span>
-            <span className={style.countdownText}>HOURS</span>
+            <span className={style.countdownText}>{t("countdown.hour")}</span>
           </div>
           <div className={style.countdownBox}>
             <span className={`${style.countdownNumber} ${style.mins}`}>
               {minutes}
             </span>
-            <span className={style.countdownText}>MINS</span>
+            <span className={style.countdownText}>{t("countdown.min")}</span>
           </div>
           <div className={style.countdownBox}>
             <span className={`${style.countdownNumber} ${style.sec}`}>
               {seconds}
             </span>
-            <span className={style.countdownText}>SEC</span>
+            <span className={style.countdownText}>{t("countdown.sec")}</span>
           </div>
         </div>
       </div>
